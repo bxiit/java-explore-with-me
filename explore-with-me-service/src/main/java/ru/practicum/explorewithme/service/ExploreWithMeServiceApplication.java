94ca75bb1076e0913cd90c4e0c5ef7c9ed78b1cb
bxiit <bexeiitatabek@yandex.kz>
2024-10-15 23:17:43 +0500

    Add: jar packaging in dockerfile ewm-stats-api

---
+++ explore-with-me-service/src/main/java/ru/practicum/explorewithme/service/ExploreWithMeServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.practicum.explorewithme.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreWithMeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreWithMeServiceApplication.class, args);
+    }
+
+}
---
+++ explore-with-me-service/src/test/java/ru/practicum/explorewithme/service/ExploreWithMeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.practicum.explorewithme.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreWithMeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
