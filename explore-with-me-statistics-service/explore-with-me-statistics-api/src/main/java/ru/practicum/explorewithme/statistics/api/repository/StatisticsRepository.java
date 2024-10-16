5511906aa13164d939bb872e5eb2129c61bcc155
bxiit <bexeiitatabek@yandex.kz>
2024-10-16 05:00:34 +0500

    Add: ilike for each uri

--- explore-with-me-statistics-service/explore-with-me-statistics-api/src/main/java/ru/practicum/explorewithme/statistics/api/repository/StatisticsRepository.java
+++ explore-with-me-statistics-service/explore-with-me-statistics-api/src/main/java/ru/practicum/explorewithme/statistics/api/repository/StatisticsRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import ru.practicum.explorewithme.statistics.api.entity.Endpoint;
 
 import java.time.Instant;
-import java.util.Collection;
 import java.util.List;
 
 public interface StatisticsRepository extends JpaRepository<Endpoint, Long> {
@@ -13,14 +12,14 @@ public interface StatisticsRepository extends JpaRepository<Endpoint, Long> {
             select e.app as app,
             e.uri as uri,
             count(e.id) as hits
-            from Endpoint e
+            from endpoint e
             where (e.timestamp between ?1 and ?2)
-            and e.uri in ?3
+            and (e.uri ilike any (array[?3]))
             group by e.app, e.uri
             having (?4 is false or count(e.id) = 1)
             order by hits desc
-            """)
-    List<ViewStatsProj> findAllWithUri(Instant start, Instant end, Collection<String> uris, boolean unique);
+            """, nativeQuery = true)
+    List<ViewStatsProj> findAllWithUri(Instant start, Instant end, String[] uris, boolean unique);
 
     @Query(value = """
             select e.app as app,
--- explore-with-me-statistics-service/explore-with-me-statistics-api/src/main/java/ru/practicum/explorewithme/statistics/api/service/StatisticsServiceImpl.java
+++ explore-with-me-statistics-service/explore-with-me-statistics-api/src/main/java/ru/practicum/explorewithme/statistics/api/service/StatisticsServiceImpl.java
@@ -14,6 +14,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.util.Collection;
 import java.util.List;
 
 @Slf4j
@@ -37,10 +38,19 @@ public class StatisticsServiceImpl implements StatisticsService {
                     .map(vs -> new ViewStats(vs.getApp(), vs.getUri(), vs.getHits()))
                     .toList();
         } else {
-            return statisticsRepository.findAllWithUri(toInstant(request.getStart()), toInstant(request.getEnd()), request.getUris(), request.isUnique()).stream()
+            return statisticsRepository.findAllWithUri(
+                            toInstant(request.getStart()),
+                            toInstant(request.getEnd()),
+                            addPercentSymbolForEach(request.getUris()).toArray(String[]::new),
+                            request.isUnique()
+                    ).stream()
                     .map(vs -> new ViewStats(vs.getApp(), vs.getUri(), vs.getHits()))
                     .toList();
         }
+    }
+
+    private List<String> addPercentSymbolForEach(Collection<String> uris) {
+        return uris.stream().map(uri -> "%" + uri + "%").toList();
     }
 
     private Instant toInstant(LocalDateTime ldt) {
