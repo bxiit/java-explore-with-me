59e05ecbc899eac9b5193605a91a4cbb4f1535ca
bxiit <bexeiitatabek@yandex.kz>
2025-02-03 14:33:34 +0500

    Fix: full class path in rate repo

--- explore-with-me-service/src/main/java/ru/practicum/explorewithme/service/repository/RateRepository.java
+++ explore-with-me-service/src/main/java/ru/practicum/explorewithme/service/repository/RateRepository.java
@@ -3,15 +3,16 @@ package ru.practicum.explorewithme.service.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.practicum.explorewithme.service.entity.EventFeedback;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface RateRepository extends JpaRepository<ru.practicum.explorewithme.service.entity.EventFeedback, Long> {
-    List<ru.practicum.explorewithme.service.entity.EventFeedback> findAllByEventId(Long eventId, Pageable pageable);
+public interface RateRepository extends JpaRepository<EventFeedback, Long> {
+    List<EventFeedback> findAllByEventId(Long eventId, Pageable pageable);
 
-    List<ru.practicum.explorewithme.service.entity.EventFeedback> findAllByEventId(Long eventId);
+    List<EventFeedback> findAllByEventId(Long eventId);
 
-    Optional<ru.practicum.explorewithme.service.entity.EventFeedback> findByEventIdAndUserId(Long eventId, Long userId);
+    Optional<EventFeedback> findByEventIdAndUserId(Long eventId, Long userId);
 }
